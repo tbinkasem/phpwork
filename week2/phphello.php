a22488955035fb16a79647030edcd8a68ec592ca
Teera Binkasem <itgit@ptnicec.ac.th>
2018-10-30 10:48:32 +0700

    แก้ไขไฟล์ phphello

--- week2/phphello.php
+++ week2/phphello.php
@@ -1,9 +1,10 @@
 <html>
     <head>
-        <title>Hello PHP World!!!!</title>
+        <title>Hello PHP World</title>
     </head>
     <body>
         <h1>TEST PHP CODE</h1>
+        <h2>TEST HTML CODE</h2>
         <?php
             echo "Hello PHP World";
         ?>
